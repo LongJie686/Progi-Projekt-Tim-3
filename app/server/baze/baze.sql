ac7dcb22a8912832425eb0491e868f6d2e849aec
stjepanmmm <stjepan.martinovic@fer.hr>
2025-11-03 20:54:24 +0100

    baza

---
+++ app/server/baze/baze.sql
@@ -0,0 +1,32 @@
+CREATE TABLE users (
+                       id SERIAL PRIMARY KEY,
+                       email VARCHAR(255) UNIQUE NOT NULL,
+                       password_hash VARCHAR(255) NOT NULL,
+                       is_professor BOOLEAN NOT NULL,
+                       name VARCHAR(100) NOT NULL,
+                       surname VARCHAR(100) NOT NULL,
+                       created_at TIMESTAMP DEFAULT CURRENTTIMESTAMP,
+                       CHECK (email ~* '^[A-Za-z0-9.%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,}$'),
+);
+
+CREATE TABLE students (
+                          user_id INT PRIMARY KEY REFERENCES users(id) ON DELETE CASCADE,
+                          sex CHAR(1),
+                          city VARCHAR(100),
+                          education VARCHAR(255),
+                          date_of_birth DATE,
+                          school VARCHAR(255),
+                          CHECK (sex IN ('M', 'F')),
+                          CHECK (date_of_birth <= CURRENT_DATE)
+);
+
+CREATE TABLE professors (
+                            user_id INT PRIMARY KEY REFERENCES users(id) ON DELETE CASCADE,
+                            sex CHAR(1),
+                            city VARCHAR(100),
+                            teaching VARCHAR(255),
+                            date_of_birth DATE,
+                            school VARCHAR(255),
+                            CHECK (sex IN ('M', 'F')),
+                            CHECK (date_of_birth <= CURRENT_DATE)
+);
