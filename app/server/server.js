ad1ac9a8ad4e1128455ce34fe1f00bc7fa5ab4b6
FranKovacevic <fk56063@fer.hr>
2025-11-11 20:59:06 +0100

    Napravio sam da backend radi, kad na prvoj stranici upises email i sifru i stisnes register onda ti se posalje mail, pa kad stisnes na link iz maila odvede te na stranicu di trebas stisnut gumb Verify koji te odvede na finish registracije di jos upises ostale podatke kao sta su ime prezime i ostalo. I onda kad tamo stisnes register onda se napravi korisnik u bazi. Rade i login i logout. Sve radi kad sam testiro u postmanu ali nece vam radit ak probate u browseru jer jos nemamo napravljen frontend.

--- app/server/server.js
+++ app/server/server.js
@@ -9,7 +9,7 @@ const session = require('express-session');
 const app = express();
 
 app.use(cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true
 }));
 
@@ -18,6 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 //cookie
+/* mozda ipak netreba al cu ostavit tu ak ce nam trebat
 app.use(session({
     secret: process.env.SESSION_SECRET || "secret",
     resave: false,
@@ -28,6 +29,7 @@ app.use(session({
         maxAge: 15 * 60 * 1000 // 15 min
     }
 }))
+*/
 
 const authRoutes = require('./routes/auth');
 app.use("/auth", authRoutes);
@@ -38,6 +40,7 @@ app.use('/profile', profRoutes);
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public/index.html"));
 });
+
 app.get("/finish-register", (req, res) => {
     res.sendFile(path.join(__dirname, "public/finish-register.html"));
 })
