4b76de64ff22732bd14a9e5fc6e409c0a30cd1f8
Jure-6ic <js56082@fer.hr>
2025-11-13 22:41:22 +0100

    Sad je sve kako je na serveru.

--- app/client/src/App.jsx
+++ app/client/src/App.jsx
@@ -5,8 +5,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import RegisterEmail from './pages/RegisterEmail';
-import FinishRegister from "./pages/FinishRegister.jsx";
-
+import FinishRegister from './pages/FinishRegister';
 // ... ostale
 
 // Uvezi layout
@@ -20,7 +19,6 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<RegisterEmail />} />
             <Route path="/finish-register" element={<FinishRegister />} />
-
             {/* Rute koje KORISTE MainLayout (s navigacijom) */}
             <Route element={<MainLayout />}>
                 <Route path="/" element={<Home />} />
