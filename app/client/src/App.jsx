280d27ad1a585230da622f4821b375800be8c2b8
Jure-6ic <js56082@fer.hr>
2025-11-13 03:13:46 +0100

    Frontend.

--- app/client/src/App.jsx
+++ app/client/src/App.jsx
@@ -5,6 +5,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import RegisterEmail from './pages/RegisterEmail';
+import FinishRegister from './pages/FinishRegister';
 // ... ostale
 
 // Uvezi layout
