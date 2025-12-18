3b1d69e7ba20b29686852600bd0ec523fde7f265
Fran <fk56063@fer.hr>
2025-12-18 13:51:20 +0100

    Vraćeno na 1c18c6cddb7368307a06b962d2a252016058035d commit da mogu napravit fix

--- app/client/vite.config.js
+++ app/client/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
         proxy: {
             // Svi zahtjevi koji počinju s '/api' bit će preusmjereni
             '/api': {
-                target: 'http://localhost:3001', // Backend server on port 3001
+                target: 'http://localhost:8080', // Backend server on port 3001
                 changeOrigin: true,
                 secure: false,
             },
